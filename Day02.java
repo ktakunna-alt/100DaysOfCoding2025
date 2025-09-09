3d4973d9ef1f02301289a24c52db6f12d6c934c1
ktakunna-alt <ktakunna@gmail.com>
2025-09-09 16:21:02 +0800

    Create Day02.java

---
+++ Day02.java
@@ -0,0 +1,14 @@
+public class Day02 {
+    public static void main(String[] args) {
+        String nama = "Kristin Takunna";
+
+        // println
+        System.out.println("Nama : " + nama);
+
+        // print
+        System.out.print("Nama : " + nama + "\n");
+
+        // printf
+        System.out.printf("Nama : %s", nama);
+    }
+}
