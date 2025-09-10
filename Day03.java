b487f9d76765999a7bd05f3afa8590ad7987c045
ktakunna-alt <ktakunna@gmail.com>
2025-09-10 16:18:07 +0800

    Create Day03.java

---
+++ Day03.java
@@ -0,0 +1,19 @@
+public class Day03 {
+    public static void main(String[] args) {
+        // Ini komentar untuk satu baris
+        String nama = "Kristin";
+        System.out.println("Nama: " + nama);
+
+        /* Ini komentar lebih dari satu baris
+           Bisa dipakai untuk catatan panjang
+           atau nonaktifin kode sementara */
+        int umur = 20;
+        System.out.println("Umur: " + umur);
+
+        /** 
+         * Ini komentar dokumentasi
+         * Biasanya dipakai buat bikin JavaDoc 
+         */
+        System.out.println("Halo, selamat belajar Java!");
+    }
+}
