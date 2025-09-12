96b7d3216ef681637f314c7c804aaaf3b24971c7
ktakunna-alt <ktakunna@gmail.com>
2025-09-12 15:47:26 +0800

    Create Day05.java

---
+++ Day05.java
@@ -0,0 +1,19 @@
+public class Day05 {
+    public static void main(String[] args) {
+        // byte
+        byte angkaByte = 100;
+        System.out.println("Nilai byte: " + angkaByte);
+
+        // short
+        short angkaShort = 20000;
+        System.out.println("Nilai short: " + angkaShort);
+
+        // int
+        int angkaInt = 1000000;
+        System.out.println("Nilai int: " + angkaInt);
+
+        // long
+        long angkaLong = 10000000000L; // ingat L di akhir angka
+        System.out.println("Nilai long: " + angkaLong);
+    }
+}
