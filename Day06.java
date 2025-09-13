845ddd8fc20f250efe05e38abdec3183aa537521
ktakunna-alt <ktakunna@gmail.com>
2025-09-13 19:54:50 +0800

    Create Day06.java

---
+++ Day06.java
@@ -0,0 +1,9 @@
+public class Day06 {
+    public static void main(String[] args) {
+        float angka = 35.8f;
+        double angka = 55.123456789;
+
+        System.out.println("Suhu tubuh (float)  : " + suhu);
+        System.out.println("Berat badan (double): " + berat);
+    }
+}
