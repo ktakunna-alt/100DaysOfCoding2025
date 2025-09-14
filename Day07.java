ebd8b737f4e5074857c47fab21b1f8c5d7c9fa33
ktakunna-alt <ktakunna@gmail.com>
2025-09-14 18:53:26 +0800

    Create Day07.java

---
+++ Day07.java
@@ -0,0 +1,9 @@
+public class Day07 {
+    public static void main(String[] args) {
+        char huruf = 'K';
+        boolean belajar = true;
+
+        System.out.println("Huruf: " + huruf);
+        System.out.println("Sedang belajar? " + belajar);
+    }
+}
