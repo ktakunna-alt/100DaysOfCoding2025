37289b53229217ce65b23dbbed8e70a3dabc44d2
ktakunna-alt <ktakunna@gmail.com>
2025-09-15 19:31:13 +0800

    Update Day08.java

--- Day08.java
+++ Day08.java
@@ -3,9 +3,16 @@ public class Day08 {
         String nama = "Kristin";
         String status = "Webtoon reader";
         String genreFav = "Horror, thriller, aksi";
-        
+
+        System.out.println("===Profil===");
         System.out.println("Nama: " + nama);
         System.out.println("Status: " + status);
         System.out.println("Genre Favorit: " + genreFav);
     }
 }
+/*Output 
+===Profil===
+Nama: Kristin 
+Status: Webtoon reader 
+Genre Favorit: Horror, thriller, aksi
+*/
