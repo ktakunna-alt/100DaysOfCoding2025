147dee03c4f11ea7e898eb258508f97da216b4aa
ktakunna-alt <ktakunna@gmail.com>
2025-09-15 19:15:01 +0800

    Create Day08.java

---
+++ Day08.java
@@ -0,0 +1,11 @@
+public class Day08 {
+    public static void main(String[] args) {
+        String nama = "Kristin";
+        String status = "Webtoon reader";
+        String genreFav = "Horror, thriller, aksi";
+        
+        System.out.println("Nama: " + nama);
+        System.out.println("Status: " + status);
+        System.out.println("Genre Favorit: " + genreFav);
+    }
+}
