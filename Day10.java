c9ced01afd453695969d3d6a6182d578afa12098
ktakunna-alt <ktakunna@gmail.com>
2025-09-17 18:21:03 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,11 @@
+public class Day10 {
+    public static void main(String[] args) {
+        final int UMUR = 20; // ini constanta dan ditulis dalam huruf kapital semua
+        final String NAMA = "Kristin";
+
+        System.out.println("Nama : " + NAMA);
+        System.out.println("Umur : " + UMUR);
+
+        // UMUR = 25; // ❌ Ini bakal error karena final. artinya nilai/valuenya tidak bisa di ganti.
+    }
+}
