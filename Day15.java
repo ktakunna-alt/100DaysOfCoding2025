f5cba9f638a5876877d39ba73d5b6043e099be7c
ktakunna-alt <ktakunna@gmail.com>
2025-09-22 19:07:05 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,14 @@
+public class Day15 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 4;
+
+        int hasilTambah = a + b;   // penjumlahan
+        int hasilKurang = a - b;   // pengurangan
+
+        System.out.println("Nilai a: " + a);
+        System.out.println("Nilai b: " + b);
+        System.out.println("Hasil a + b = " + hasilTambah);
+        System.out.println("Hasil a - b = " + hasilKurang);
+    }
+}
