02b476606543353f47604730e4ca4320e02b88b2
ktakunna-alt <ktakunna@gmail.com>
2025-09-23 21:33:56 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,9 @@
+public class Day16 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+
+        System.out.println(a * b); // hasil perkalian
+        System.out.println(a / b); // hasil pembagian
+    }
+}}
