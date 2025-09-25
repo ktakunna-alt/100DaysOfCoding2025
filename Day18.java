13a999ac1fc26a02ef60121a6d261149b8242266
ktakunna-alt <ktakunna@gmail.com>
2025-09-25 22:09:12 +0800

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,9 @@
+public class Day18 {
+    public static void main(String[] args) {
+        byte kecil = 50;       // 1 byte
+        short besar = kecil;   // otomatis naik ke short (2 byte)
+
+        System.out.println("Nilai byte: " + kecil);
+        System.out.println("Nilai short: " + besar);
+    }
+}
