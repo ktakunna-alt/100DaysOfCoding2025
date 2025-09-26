84077f90cb39cf659cfd76f9e808e9f8481e1afb
ktakunna-alt <ktakunna@gmail.com>
2025-09-26 21:50:51 +0800

    Create Day19.java

---
+++ Day19.java
@@ -0,0 +1,7 @@
+public class Day19 {
+    public static void main(String[] args) {
+        int angkaInt = 130;               
+        byte angkaByte = (byte) angkaInt;
+        System.out.println("int 130 ke byte: " + angkaByte);
+    }
+}
