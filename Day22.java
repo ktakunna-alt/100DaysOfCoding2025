dbb14b7b95aee5087b458323be0a6010fe4c0575
ktakunna-alt <ktakunna@gmail.com>
2025-09-29 22:04:58 +0800

    Create Day22.java

---
+++ Day22.java
@@ -0,0 +1,9 @@
+public class Day22 {
+    public static void main(String[] args) {
+        int sisi = 5; // panjang sisi persegi
+        int luas = sisi * sisi; // rumus luas persegi
+
+        System.out.println("Panjang sisi: " + sisi);
+        System.out.println("Luas persegi: " + luas);
+    }
+}
