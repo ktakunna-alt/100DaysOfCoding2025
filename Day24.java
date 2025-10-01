00207b23d1ce4aa932a3452f30607fb0eafda4da
ktakunna-alt <ktakunna@gmail.com>
2025-10-01 23:38:37 +0800

    Create Day24.java

---
+++ Day24.java
@@ -0,0 +1,8 @@
+public class Day24 {
+    public static void main(String[] args) {
+        double r = 7; // jari-jari lingkaran
+        double luas = 3.14 * r * r; // rumus luas lingkaran πr²
+
+        System.out.println("Luas lingkaran = " + luas);
+    }
+}
