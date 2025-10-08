2a9cc0b44b2b51dc398963e3c1b6763a74b4b92e
ktakunna-alt <ktakunna@gmail.com>
2025-10-08 22:49:52 +0800

    Update Day31.java

--- Day31.java
+++ Day31.java
@@ -1,17 +1,10 @@
-import java.util.Scanner;
-
 public class Day31 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        int a = 5;
+        int b = 10;
 
-        System.out.print("Masukkan nilai ujian teori: ");
-        int teori = input.nextInt();
+        boolean hasil = (a > 0) && (b > a);
 
-        System.out.print("Masukkan nilai ujian praktik: ");
-        int praktik = input.nextInt();
-
-        boolean lulus = (teori >= 70) && (praktik >= 70);
-
-        System.out.println("Lulus: " + lulus);
+        System.out.println("Hasil: " + hasil);
     }
 }
