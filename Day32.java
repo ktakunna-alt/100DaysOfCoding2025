188e1a034704d4bc1a791ac22296b25b81706384
ktakunna-alt <ktakunna@gmail.com>
2025-10-09 21:46:49 +0800

    Update Day32.java

--- Day32.java
+++ Day32.java
@@ -1,7 +1,7 @@
 public class Day32 {
     public static void main(String[] args) {
         int a = 10;
-        int b = 5;
+        int b = 2;
         boolean hasil = (a > 5 || b > 10);
         System.out.println("Apakah salah satu kondisi terpenuhi? " + hasil);
     }
