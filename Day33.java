cbf2cf49ce61cd674e717546651e5cd50b318f94
ktakunna-alt <ktakunna@gmail.com>
2025-10-10 23:10:17 +0800

    Create Day33.java

---
+++ Day33.java
@@ -0,0 +1,11 @@
+public class Day33 {
+    public static void main(String[] args) {
+        boolean lapar = false;
+
+        if (!lapar) {
+            System.out.println("Kamu sudah kenyang!");
+        } else {
+            System.out.println("Kamu lapar, ayo makan!");
+        }
+    }
+}
