1a0347864db1fad3dae86876dcb12787f75d5e40
ktakunna-alt <ktakunna@gmail.com>
2025-10-12 23:31:33 +0800

    Create Day35.java

---
+++ Day35.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Day35 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai ujian kamu: ");
+        int nilai = input.nextInt();
+
+        if (nilai >= 90) {
+            System.out.println("Nilai kamu A. Sangat baik!");
+        } else if (nilai >= 75) {
+            System.out.println("Nilai kamu B. Baik.");
+        } else if (nilai >= 60) {
+            System.out.println("Nilai kamu C. Cukup.");
+        } else if (nilai >= 45) {
+            System.out.println("Nilai kamu D. Kurang.");
+        } else {
+            System.out.println("Nilai kamu E. Tidak lulus.");
+        }
+
+        input.close();
+    }
+}
