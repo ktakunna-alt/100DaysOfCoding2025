e8772fbd348fa861932364c0162b6f5a76107f57
ktakunna-alt <ktakunna@gmail.com>
2025-10-21 22:48:54 +0800

    Update Day44.java

--- Day44.java
+++ Day44.java
@@ -1,24 +1,32 @@
+import java.util.Scanner;
+
 public class Day44 {
     public static void main(String[] args) {
-        
-        int nilai = 78;
-        
-        char predikat; 
-      
-        if (nilai >= 85) {
-            predikat = 'A';
-        } else if (nilai >= 75) {
-            predikat = 'B';
-        } else if (nilai >= 65) {
-            predikat = 'C';
-        } else if (nilai >= 50) {
-            predikat = 'D';
-        } else {
-            predikat = 'E';
-        }
 
-        
-        System.out.println("Nilai: " + nilai);
-        System.out.println("Predikat: " + predikat);
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai: ");
+        int nilai = input.nextInt();
+
+        if (nilai < 0 || nilai > 100) {
+            System.out.println("Nilai tidak valid. masukkan antara 0-100.");
+        } else {
+            char predikat;
+
+            if (nilai >= 85) {
+                predikat = 'A';
+            } else if (nilai >= 75) {
+                predikat = 'B';
+            } else if (nilai >= 65) {
+                predikat = 'C';
+            } else if (nilai >= 50) {
+                predikat = 'D';
+            } else {
+                predikat = 'E';
+            }
+
+            System.out.println("Nilai: " + nilai);
+            System.out.println("Predikat: " + predikat);
+        }
     }
 }
