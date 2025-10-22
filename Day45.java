6526dbe31f7c3eab1576712f6b7f3710bab6fc23
ktakunna-alt <ktakunna@gmail.com>
2025-10-22 22:55:27 +0800

    Rename class from KasusDetektifSelesai to Day45

--- Day45.java
+++ Day45.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class KasusDetektifSelesai {
+public class Day45 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
