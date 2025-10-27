a480276319a38ebf16cc8492395be3db7ca23010
ktakunna-alt <ktakunna@gmail.com>
2025-10-27 22:43:47 +0800

    Add Day49 class to check even or odd numbers

---
+++ Day49.java
@@ -0,0 +1,9 @@
+public class Day49 {
+    public static void main(String[] args) {
+        int angka = 12;
+
+        String hasil = (angka % 2 == 0) ? "Genap" : "Ganjil";
+
+        System.out.println("Angka " + angka + " adalah bilangan " + hasil);
+    }
+}
