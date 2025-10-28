065ee410bff45c4e7fcdb5c5c11124893ca6c6fa
ktakunna-alt <ktakunna@gmail.com>
2025-10-28 23:42:14 +0800

    Create Day51.java

---
+++ Day51.java
@@ -0,0 +1,8 @@
+public class Day51 {
+    public static void main(String[] args) {
+        // Perulangan dari angka 1 sampai 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Angka ke-" + i);
+        }
+    }
+}
