722e497ddd045ec424dce3101e3dcae65e43f698
ktakunna-alt <ktakunna@gmail.com>
2025-11-02 23:46:15 +0800

    Update Day56.java

--- Day56.java
+++ Day56.java
@@ -2,34 +2,36 @@ import java.util.Scanner;
 
 public class Day56 {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
         String username, password;
         int percobaan = 0;
         boolean loginBerhasil = false;
 
         do {
-            System.out.print("Masukkan Username: ");
+            System.out.print("Masukkan Username : ");
             username = input.nextLine();
 
-            System.out.print("Masukkan Password: ");
+            System.out.print("Masukkan Password : ");
             password = input.nextLine();
 
             if (username.equalsIgnoreCase("Kristin") && password.equals("D0224329")) {
-                System.out.println("Login Sebagai Pengguna");
+                System.out.println("\nLogin Sebagai Pengguna\n");
                 loginBerhasil = true;
-            } else if (username.equalsIgnoreCase("admin") && password.equalsIgnoreCase("admin")) {
-                System.out.println("Login Sebagai Admin");
-                loginBerhasil = true;
-            } else {
-                System.out.println("Silahkan Coba Lagi");
-                percobaan++;
-            }
 
-            if (percobaan == 3 && !loginBerhasil) {
-                System.out.println("Login Gagal");
+            } else if (username.equalsIgnoreCase("admin") && password.equalsIgnoreCase("admin")) {
+                System.out.println("\nLogin Sebagai Admin\n");
+                loginBerhasil = true;
+
+            } else {
+                System.out.println("\nSilahkan coba lagi.\n");
+                percobaan++;
             }
 
         } while (!loginBerhasil && percobaan < 3);
 
+        if (!loginBerhasil) {
+            System.out.println("Login Gagal.\n");
+        }
     }
 }
